b15752641023870f5c36933c2584488a6797e922
Ole Hagen <ole_hagen96@hotmail.com>
2023-06-22 09:54:52 +0200

    refactor(README.md): update project directory name in README to reflect new project name feat(app.py): change prompt generation to accept name and category inputs instead of animal input feat(app.py): update prompt to include more name options and higher temperature for more creative names refactor(index.html): update page title and input labels to reflect new prompt inputs

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ from flask import Flask, redirect, render_template, request, url_for
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        name = request.form["name"]
+        category = request.form["category"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+            prompt=generate_prompt(name, category),
+            temperature=0.8,
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
@@ -22,14 +22,15 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+def generate_prompt(name, category):
+    # Change this to make the prompt your own!
+    prompt = ("""Suggest three names for a {ctg}
+                name: Ole
+                Names: Captain Ole the {ctg}, Agent-{ctg} Ole, Ole The Incredible {ctg}
+                name: Jorgen
+                Names: {ctg} Jorgen, Ol' Jorgen the {ctg}, Jorgen the Great {ctg}
+                name: Daniel
+                Names: {ctg} Daniel, Daniel the Magnificent {ctg}, Daniel Mc{ctg}
+                name: {nm}
+                Names:""").format(ctg=category, nm=name)
+    return prompt
